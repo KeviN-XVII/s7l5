c262627105d29807fa21ffbb8dc7a0bb109e0145
KeviN-XVII <kevinquartar@gmail.com>
2026-02-20 09:58:48 +0100

    Initial commit

---
+++ src/main/java/kevinquarta/s7l5/S7l5Application.java
@@ -0,0 +1,13 @@
+package kevinquarta.s7l5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7l5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7l5Application.class, args);
+	}
+
+}
---
+++ src/test/java/kevinquarta/s7l5/S7l5ApplicationTests.java
@@ -0,0 +1,13 @@
+package kevinquarta.s7l5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7l5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
